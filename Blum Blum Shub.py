ca71f1baa7a7e149563879a0cec46201b7aa53ab
CM01102 <93478417+CM01102@users.noreply.github.com>
2021-11-04 11:41:45 +0200

    Update Blum Blum Shub.py

--- Blum Blum Shub.py
+++ Blum Blum Shub.py
@@ -1,4 +1,5 @@
 algorithm=str(input("Please enter your preferred algorithm between BBS, TRN, or File: "))
+
 #BBS Code
 if algorithm=="BBS":
     def seedbbs(initialvalue):
@@ -11,16 +12,14 @@ if algorithm=="BBS":
      global random
      random = (random**2) % M
      return random
- 
     seedbbs(int(input("Please enter initial value: ")))
-    
     for i in range(12):
      print(int(BBS()))
+    
  #TRN Code    
 elif algorithm=="TRN":
     import time
     seconds = time.time()
-
     def seedbbs(initialvalue):
      global random
      random = initialvalue
@@ -31,11 +30,10 @@ elif algorithm=="TRN":
      global random
      random = (random**2) % M
      return random
-
     seedbbs(seconds)
-
     for i in range(12):
      print(int(BBS()))
+    
 #File Code
 elif algorithm=="File":
     file_name = str(input("Please specify your file name followed by the file extension: "))
@@ -43,7 +41,6 @@ elif algorithm=="File":
     def file_window():
      global file_name
      file_name=seed.askopenfilename() 
-     
     def seedbbs(initialvalue):
      global random
      random = initialvalue
@@ -54,12 +51,10 @@ elif algorithm=="File":
      global random
      random = (random**2) % M
      return random
- 
     with open(file_name,"r")as file:
-     seedbbs(int(file.readline()))
-     
+     seedbbs(int(file.readline())) 
     for i in range(12):
      print(int(BBS()))
+    
 else:
     print("Please try again :) ")
-
